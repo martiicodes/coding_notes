9c12760d4261adfe5da9710c74816095a1b882fa
Nenad Milcanovic <milcanovicn@gmail.com>
2024-08-02 07:53:03 +0200

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,11 @@
+# Can't concatenate strigns with integers.
+
+print("Zdravo, koliko imaš godina?")
+      
+godine = input()  
+
+#print("Hej, pa ti imaš" + (string(int(godine)) + 1) + "sledeće godine!")
+
+print("Hej, imaćeš " + str(int(godine) + 1) + " sledeće godine!") 
+
+#str(int(myAge) + 1)
---
+++ len_hello.py
@@ -0,0 +1,6 @@
+len('hello')
+print(len('hello'))
+
+len('My very energetic monster just scarfed nachos.')
+print(len('My very energetic monster just scarfed nachos.'))
+len('')
