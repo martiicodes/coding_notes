e92feb2c5203a970d74a8226986a6bfd7a5fd3ee
Nenad Milcanovic <milcanovicn@gmail.com>
2024-08-03 08:23:12 +0200

    Add files via upload

---
+++ an_annoying_while_loop.py
@@ -0,0 +1,7 @@
+name = ''
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
+
+# Here’s a small example program that will keep asking you to type, literally, your name. 
