05351455270b1edefb79976f7bcfa4ae1b8c03ac
Nenad Milcanovic <milcanovicn@gmail.com>
2024-08-03 10:15:16 +0200

    Add files via upload

---
+++ five_times_while_loop.py
@@ -0,0 +1,8 @@
+# Let’s rewrite fiveTimes.py to use a while loop equivalent of a for loop.
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1
+
