accb081243759c2c3b8ac4cd76a3eaba2356f4ac
Nenad Milcanovic <milcanovicn@gmail.com>
2024-08-03 10:10:23 +0200

    Add files via upload

---
+++ carl_friedrich_gauss.py
@@ -0,0 +1,8 @@
+total = 0
+for num in range(101):
+    total = total + num
+print(total)  
+
+# (Young Gauss figured out a way to solve the problem in seconds. 
+# There are 50 pairs of numbers that add up to 101: 1 + 100, 2 + 99, 3 + 98, and so on, until 50 + 51. 
+# Since 50 × 101 is 5,050, the sum of all the numbers from 0 to 100 is 5,050. Clever kid!)
