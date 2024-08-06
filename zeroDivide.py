68e5e99aee9caee7bb4c5ab122b96ce6730ca3c4
Nenad Milcanovic <milcanovicn@gmail.com>
2024-08-06 09:26:37 +0200

    Add files via upload

---
+++ zeroDivide.py
@@ -0,0 +1,10 @@
+def spam(divideBy):
+    return 42 / divideBy
+
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
+
+# A ZeroDivisionError happens whenever you try to divide a number by zero.
+
