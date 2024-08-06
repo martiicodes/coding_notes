77bb3f52b0711e119b7a83876f2933c6431e20b6
Nenad Milcanovic <milcanovicn@gmail.com>
2024-08-06 09:40:26 +0200

    Add files via upload

---
+++ zigzag.py
@@ -0,0 +1,29 @@
+# Let’s use the programming concepts you’ve learned so far to create a small animation program. 
+# This program will create a back-and-forth, zigzag pattern until the user stops it by pressing 
+# the Mu editor’s Stop button or by pressing CTRL-C.
+
+import time, sys
+indent = 0 # How many spaces to indent.
+indentIncreasing = True # Whether the indentation is increasing or not.
+
+try:
+    while True: # The main program loop.
+        print(' ' * indent, end='')
+        print('********')
+        time.sleep(0.1) # Pause for 1/10 of a second.
+
+        if indentIncreasing:
+            # Increase the number of spaces:
+            indent = indent + 1
+            if indent == 20:
+                # Change direction:
+                indentIncreasing = False
+
+        else:
+            # Decrease the number of spaces:
+            indent = indent - 1
+            if indent == 0:
+                # Change direction:
+                indentIncreasing = True
+except KeyboardInterrupt:
+    sys.exit()
